b6b89681a2e3bbaf921bcc2217ee819ebbfb6710
Tim Heuett <tim.heuett@gmail.com>
2025-10-05 13:52:59 -0700

    fix merge conflict duplicates

--- src/world/score_keeper_utility.gd
+++ src/world/score_keeper_utility.gd
@@ -1,7 +1,5 @@
 extends Node
 
-var Score: float = 0.0
-var DisplayedScore: float = 0.0
 var Score: int = 0
 var DisplayedScore: int = 0
 
@@ -10,6 +8,5 @@ func _process(delta) -> void:
 		DisplayedScore = DisplayedScore + ceil(delta * (Score - DisplayedScore) * 0.2)
 		DisplayedScore = DisplayedScore + ceil(delta * (Score - DisplayedScore))
 
-func _add_score(addedScore: float) -> void:
 func _add_score(addedScore: int) -> void:
 	Score = Score + addedScore
