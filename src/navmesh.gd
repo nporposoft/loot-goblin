0a565e0fa64808a3c78d39671be2affffa7317d2
Tim Heuett <tim.heuett@gmail.com>
2025-10-04 19:19:27 -0700

    nav mesh covers entire map

--- src/navmesh.gd
+++ src/navmesh.gd
@@ -14,11 +14,12 @@ func _ready():
 
 func _bake_nav_mesh() -> void:
 	var tile_size: int = map.tile_set.tile_size.x
+	var map_rect: Rect2 = map.get_used_rect()
 	navigation_polygon.add_outline(PackedVector2Array([
-		Vector2(0, 0),
-		Vector2(map.width * tile_size, 0),
-		Vector2(map.width * tile_size, map.height * tile_size),
-		Vector2(0, map.height * tile_size)
+		Vector2(map_rect.position.x, map_rect.position.y) * tile_size,
+		Vector2(map_rect.end.x, map_rect.position.y) * tile_size,
+		Vector2(map_rect.end.x, map_rect.end.y) * tile_size,
+		Vector2(map_rect.position.x, map_rect.end.y) * tile_size
 	]))
 	bake_navigation_polygon()
 
