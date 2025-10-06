b5e94edd3048140a7415151856a8d3ea2a2eb383
Tim Heuett <tim.heuett@gmail.com>
2025-10-06 12:35:22 -0700

    finish main menu

--- src/world/scene_change_area.gd
+++ src/world/scene_change_area.gd
@@ -1,13 +1,16 @@
+class_name SceneChangeArea
 extends Detector
 
-var dungeon: PackedScene = load("res://test_dungeon.tscn")
+@export var next_scene: PackedScene
+@export var transition_time: float = 0.0
 
 var transition_started: bool = false
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not transition_started:
 		for c in get_characters():
 			if c._get_faction() == c.Faction.GOBLIN:
+				print("Beginning transition to next scene: ", next_scene)
 				begin_transition()
 				transition_started = true
 
@@ -17,9 +20,10 @@ func begin_transition() -> void:
 	add_child(end_timer)
 	end_timer.one_shot = true
 	end_timer.connect("timeout", load_dungeon)
-	end_timer.start(10.0)
+	end_timer.start(transition_time)
 
 
 func load_dungeon() -> void:
+	print("Loading next scene: ", next_scene)
 	ScoreKeeper._reset_score()
-	get_tree().change_scene_to_packed(dungeon)
+	get_tree().change_scene_to_packed(next_scene)
